2a4f4ed2f9a8979d22b645460accafc4d4895582
alexcbrownjd <alexcbrownjd@gmail.com>
2026-08-30 19:38:35 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Standard rock-solid Vite + React config
-export default defineConfig({
-  plugins: [react()],
-});
